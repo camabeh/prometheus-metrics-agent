7ff7c844ed348a83a05157f137b479fc5ebc152d
camabeh <camabeh@gmail.com>
2018-07-08 19:41:25 +0200

    Bug in debugger

--- prometheus-metrics-agent-core/src/main/java/com/fleury/metrics/agent/Agent.java
+++ prometheus-metrics-agent-core/src/main/java/com/fleury/metrics/agent/Agent.java
@@ -4,7 +4,9 @@ import com.fleury.metrics.agent.config.ArgParser;
 import com.fleury.metrics.agent.config.Configuration;
 import com.fleury.metrics.agent.reporter.PrometheusMetricSystemFactory;
 import com.fleury.metrics.agent.transformer.AnnotatedMetricClassTransformer;
+import io.prometheus.client.exporter.HTTPServer;
 
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 
 import static com.fleury.metrics.agent.config.LoggerUtil.initializeLogging;
@@ -15,8 +17,8 @@ import static com.fleury.metrics.agent.config.LoggerUtil.initializeLogging;
  */
 public class Agent {
 
-    public static void premain(String args, Instrumentation instrumentation) throws InterruptedException {
-
+    public static void premain(String args, Instrumentation instrumentation) throws InterruptedException, IOException {
+        new HTTPServer(9898);
         ArgParser argParser = new ArgParser(args);
 
         initializeLogging(argParser.getLogConfigFilename());
